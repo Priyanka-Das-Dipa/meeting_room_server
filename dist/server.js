0d32ecd6d7fc467bbedc30a47c1ef3dfc4a27b4e
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2025-01-14 23:29:40 +0600

    eslint config successful

--- dist/server.js
+++ dist/server.js
@@ -19,8 +19,8 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(config_1.default.db_url);
-            app_1.default.listen(config_1.default.db_url, () => {
-                console.log(`Example app listening on port ${config_1.default.db_url}`);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
             });
         }
         catch (error) {
--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,9 @@ import express from "express";
 import { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Meeting Room Server is Running!");
 });
-
-
 
 export default app;
