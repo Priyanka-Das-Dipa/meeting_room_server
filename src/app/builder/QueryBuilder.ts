999fe11e99f15142617d5e9050d360b3ac8c2d55
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2025-01-15 14:19:32 +0600

    queary builder write search, range, filter, range, capacity,limit, sort

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -8,6 +8,7 @@ class QueryBuilder<T> {
     this.modelQuery = modelQuery;
     this.query = query;
   }
+  
 
   // search function
   search(searchableFields: string[]) {
@@ -30,23 +31,71 @@ class QueryBuilder<T> {
     const queryObj = { ...this.query };
 
     // Filtering
-    const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
+    const excludeFields = [
+      "searchTerm",
+      "sort",
+      "limit",
+      "range",
+      "page",
+      "fields",
+      "capacity",
+      "roomsId",
+    ];
     excludeFields.forEach((el) => delete queryObj[el]);
     this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
     return this;
   }
 
+  // range
+  range() {
+    const range = this.query.range;
+    if (range) {
+      const newRange = (range as string).split("-");
+      const lowestPrice = Number(newRange[0]);
+      const highestPrice = Number(newRange[1]);
+      this.modelQuery = this.modelQuery.find({
+        pricePerSlot: { $gte: lowestPrice, $lte: highestPrice },
+      });
+    }
+    return this;
+  }
+
+  // Capacity
+
+  capacity() {
+    const capacity = this?.query?.capacity;
+    if (capacity) {
+      const newCapacity = (capacity as string).split("-");
+      const lowestPrice = Number(newCapacity[0]);
+      const highestPrice = Number(newCapacity[1]);
+      this.modelQuery = this.modelQuery.find({
+        capacity: { $gte: lowestPrice, $lte: highestPrice },
+      });
+    }
+    return this;
+  }
+
+  //   Sorting
   sort() {
+    // const sort = this?.query?.sort || "-createdAt";
     const sort =
       (this?.query?.sort as string)?.split(",")?.join(" ") || "-createdAt";
     this.modelQuery = this.modelQuery.sort(sort as string);
 
     return this;
   }
+  //  Limit
 
+  limit() {
+    const limit = Number(this?.query?.limit || 6);
+    this.modelQuery = this.modelQuery.find().limit(limit as number);
+    return this;
+  }
+
+  //   Pagination
   paginate() {
     const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 10;
+    const limit = Number(this?.query?.limit) || 6;
     const skip = (page - 1) * limit;
 
     this.modelQuery = this.modelQuery.skip(skip).limit(limit);
@@ -54,13 +103,26 @@ class QueryBuilder<T> {
     return this;
   }
 
-//   fields() {
-//     const fields =
-//       (this?.query?.fields as string)?.split(",")?.join(" ") || "-__v";
+  async countTotal() {
+    const totalQuery = this.modelQuery.getFilter();
+    const total = await this.modelQuery.model.countDocuments(totalQuery);
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 6;
+    const totalPage = Math.ceil(total / limit);
+    return {
+      page,
+      limit,
+      total,
+      totalPage,
+    };
+  }
+  //   fields() {
+  //     const fields =
+  //       (this?.query?.fields as string)?.split(",")?.join(" ") || "-__v";
 
-//     this.modelQuery = this.modelQuery.select(fields);
-//     return this;
-//   }
+  //     this.modelQuery = this.modelQuery.select(fields);
+  //     return this;
+  //   }
 }
 
 export default QueryBuilder;
