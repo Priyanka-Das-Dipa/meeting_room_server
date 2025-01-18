1ba53a1c8d69d2b1963f458ad09bd86baf9cb23b
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2025-01-18 21:53:31 +0600

    fixed the connection refuse problem

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import globalErrorhandler from "./app/middleWare/globalErrorhandler";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173/"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 
 app.use("/api", router);
