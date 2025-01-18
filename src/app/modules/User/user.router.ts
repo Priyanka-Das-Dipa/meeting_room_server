b9499048bef5913fee2b162225592ae1bff4a5e3
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2025-01-18 22:40:03 +0600

    update slot route

--- src/app/modules/User/user.router.ts
+++ src/app/modules/User/user.router.ts
@@ -1,9 +1,10 @@
-import express from "express"
+import express from "express";
 import { userController } from "./user.controller";
 
 const router = express.Router();
 
-router.get("/",  userController.getAllUsers);
-router.delete("/:id",  userController.deleteUser);
+router.get("/", userController.getAllUsers);
+// router.post("/",  userController.getAllUsers);
+router.delete("/:id", userController.deleteUser);
 
 export const userRouters = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { userRouters } from "../modules/User/user.router";
 import { bookingsRouter } from "../modules/Booking/booking.route";
 import { roomRoutes } from "../modules/Room/room.route";
 import { authRoutes } from "../modules/Auth/auth.route";
+import { slotRoute } from "../modules/Slot/slot.route";
 
 const router = express.Router();
 
@@ -17,6 +18,11 @@ const moduleRoutes = [
     path: "/rooms",
     route: roomRoutes,
   },
+  // slots
+  {
+    path: "/slots",
+    route: slotRoute,
+  },
   //   user routes
   {
     path: "/users",
@@ -27,7 +33,6 @@ const moduleRoutes = [
     path: "/auth",
     route: authRoutes,
   },
-  
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
